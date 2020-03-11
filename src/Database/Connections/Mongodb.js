ec309244ef6e173e0570d58b9471931e83a9ce00
André Glatz <andre.glatz94@gmail.com>
2020-03-11 16:55:25 -0300

    Update Mongodb.js

--- src/Database/Connections/Mongodb.js
+++ src/Database/Connections/Mongodb.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 try {
     mongoose.connect(process.env.URLMONGO, { useNewUrlParser: true, useUnifiedTopology: true });
 } catch (e) {
-    mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true });
+    mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
 module.exports = mongoose;
