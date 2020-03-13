097d231a72a7ba389d32c9f7dad1ac3dc3143ca1
André Glatz <andre.glatz94@gmail.com>
2020-03-12 22:12:07 -0300

    Update Mongodb.js
    Altered connection struct

--- src/Database/Connections/Mongodb.js
+++ src/Database/Connections/Mongodb.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 var mongoose = require('mongoose');
 
-try {
-    mongoose.connect(process.env.URLMONGO, { useNewUrlParser: true, useUnifiedTopology: true });
-} catch (e) {
-    mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
-}
+mongoose.connect(process.env.URLMONGO, { useNewUrlParser: true, useUnifiedTopology: true }).catch((e) => {
+    mongoose.connect('mongodb://localhost:27017/tasknotes', { useNewUrlParser: true, useUnifiedTopology: true }).catch((e) => {
+        console.log(e);
+    });
+});
+
+
+
 
 module.exports = mongoose;
